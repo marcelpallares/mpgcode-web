45a06b1873850df2d1164b1ec05818e2c13008bd
Marcel Pallarés <marcel.garcia@global.com>
2021-03-11 08:12:36 +0000

    Added copy-webpack-client

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 const { i18n } = require("./next-i18next.config");
 
 const withMDX = require("@next/mdx")({
@@ -13,6 +15,22 @@ module.exports = withMDX({
         fs: "empty",
       };
     }
+
+    //Copy files from posts and move them to the public folder
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, "posts"),
+            to: path.join(__dirname, "public/posts"),
+            globOptions: {
+              ignore: ["**/*.mdx"],
+            },
+          },
+        ],
+      })
+    );
+
     return config;
   },
 });
