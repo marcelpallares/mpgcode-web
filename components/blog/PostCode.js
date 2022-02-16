4b992409d2ead515261a95029040b1afd87e9e3f
marcelpallares <marcelpallaresgarcia@gmail.com>
2022-02-16 08:04:57 +0000

    Add Kotlin language

--- components/blog/PostCode.js
+++ components/blog/PostCode.js
@@ -6,13 +6,13 @@ import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 // import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
 // import applescript from "react-syntax-highlighter/dist/cjs/languages/hljs/applescript";
 // import shell from "react-syntax-highlighter/dist/cjs/languages/hljs/shell";
-// import kotlin from "react-syntax-highlighter/dist/cjs/languages/hljs/kotlin";
+import kotlin from "react-syntax-highlighter/dist/cjs/languages/hljs/kotlin";
 
 // SyntaxHighlighter.registerLanguage("language-xml", xml);
 // SyntaxHighlighter.registerLanguage("language-javascript", javascript);
 // SyntaxHighlighter.registerLanguage("language-applescript", applescript);
 // SyntaxHighlighter.registerLanguage("language-shell", shell);
-// SyntaxHighlighter.registerLanguage("language-kotlin", kotlin);
+SyntaxHighlighter.registerLanguage("language-kotlin", kotlin);
 
 const PostCode = ({ className, ...otherProps }) => {
   return (
